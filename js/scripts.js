d9a5f617e83faa3adb0dce5347af19158a6f0eb2
victoria okumu <victoria.okumu@student.moringaschool.com>
2021-06-27 10:39:18 +0300

    Added getToppingsPrice function to the object

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,22 @@ Pizza.prototype.getCrustPrice = function(){
     }
     return crustPrice;
 }
+Pizza.prototype.getToppingsPrice = function(){
+    toppingPrice=0;
+    vegToppings=['Pineapple','Mushrooms','Spinach','Olives','Hot chillies','Onions','Broccoli','Sweet Corn','Garlic']
+    meatTopings=['Shrimps','Chicken','Tuna','Bacon','Ham']
+    toppings=this.pizzaToppings
+    for(i=0; i<toppings.length;i++){
+        if(vegToppings.includes(toppings[i])){
+            toppingPrice=toppingPrice+200;
+        }
+        else if(meatTopings.includes(toppings[i])){
+            toppingPrice=toppingPrice+300;
+
+        }
+    }
+    return toppingPrice;
+}
 $(document).ready(function(){
     $('#myForm').submit(function(event){
         event.preventDefault();
